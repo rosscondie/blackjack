aa0af86be9cbca4ac432773243dbc69ef3f11373
Ross Condie <ross.condie@vier.ai>
2024-07-10 15:42:30 +0200

    add newcard logic

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
-let firstCard = 2;
-let secondCard = 11;
-let cards = [firstCard, secondCard];
-
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
+let message = "";
 
-let message = '';
-let messageEl = document.getElementById('message-el');
-let sumEl = document.getElementById('sum-el');
-let cardsEl = document.getElementById('cards-el');
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+let player = {
+  name: "Ross",
+  chips: 150,
+};
+
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
+
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
+}
 
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
   renderGame();
 }
 
 function renderGame() {
-  cardsEl.textContent = 'Cards: ' + cards[0] + ' ' + cards[1];
-  sumEl.textContent = 'Sum: ' + sum;
+  cardsEl.textContent = "Cards: ";
+
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
+  sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
-    message = 'Do you want to draw a new card?';
+    message = "Do you want to draw a new card?";
   } else if (sum === 21) {
     message = "You've got Blackjack!";
     hasBlackJack = true;
@@ -31,8 +57,10 @@ function renderGame() {
 }
 
 function newCard() {
-  let card = 8;
-  sum += card;
-  cards.push(card);
-  renderGame();
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
 }
